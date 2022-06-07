dc4ee77d9089cb2335160d04f893ee0f295b22f0
Volkerink <105924722+Volkerink@users.noreply.github.com>
2022-06-07 15:53:54 +0200

    Update GroceriesController.php
    some cleanup

--- app/Http/Controllers/GroceriesController.php
+++ app/Http/Controllers/GroceriesController.php
@@ -8,24 +8,26 @@ use Illuminate\Http\Request;
 class GroceriesController extends Controller
 {   
     public function validateInput()    {
-    return request()->validate([
-        'name' => 'required|string|min:2',
-        'category' => 'required|string|min:2',
-        'number' => 'required|integer|min:1',
-        'price' => 'required|numeric|min:0'
-    ]);
+        return request()->validate([
+            'name' => 'required|string|min:2',
+            'category' => 'required|string|min:2',
+            'number' => 'required|integer|min:1',
+            'price' => 'required|numeric|min:0'
+        ]);
     }
 
+    
     public function groceryDB() {
         return Grocery::all(); 
     }
 
+
     public function index() {
-    $groceryDB = $this->groceryDB();
-    $totalPrice = 0;
-    foreach ($groceryDB as $value) {
-        $totalPrice += ($value["price"] * $value["number"]);
-    }
+        $groceryDB = $this->groceryDB();
+        $totalPrice = 0;
+        foreach ($groceryDB as $value) {
+            $totalPrice += ($value["price"] * $value["number"]);
+        }
         $finalPrice = array(0 => number_format($totalPrice, 2, ',', ' '));
         return view('groceries.index', compact('groceryDB', 'finalPrice'));
     }
@@ -43,7 +45,6 @@ class GroceriesController extends Controller
 
 
     public function edit(Request $request) {
-        //$groceryDB = Grocery::all(); 
         $activeRow = $this->groceryDB()[$request->keys()[1] - 1];
         return view('groceries.edit', compact('activeRow'));
     }
